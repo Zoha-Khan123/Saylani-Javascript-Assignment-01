65c85564496c8cf7f7b63983b6faf73513f2c68a
Zoha Khan <zohakhan11004@gmail.com>
2024-10-12 22:59:11 +0500

    Chapter-01 to 05

---
+++ Assignment-01/Chapter-01 to 04/script.js
@@ -0,0 +1,121 @@
+//============================ Chapter 01 =====================================
+
+//Question 1 and 2
+// alert("Error! Please enter a valid password.");
+
+//Question 3
+// alert("Welcome to JS Land...\n Happy Coading!");
+
+//Question 4
+// alert("Welcome to JS Land");
+// alert("Happy Coading! \n Prevent this page from creating additional dialogs")
+
+//Question 5
+// alert("Hello.... I can run JS through my web browser's console");
+// console.log("Hello.... I can run JS through my web browser's console");
+
+//Question 6
+//------- Half answer in html file -------------
+// window.onload = function() {
+//   alert("Welcome to the page!");
+// };
+
+//Question 7
+//Answer In Html File
+
+//================================= Chapter 02 ==================================================
+
+//Question 1
+// let username;
+
+//Question 2
+// let myName = "Zoha Khan";
+
+//Question 3
+// let message = "Hello World";
+// alert(message);
+
+//Question 4
+// let studentName = "Jhon Doe";
+// let age = 15;
+// let course = "Certified Mobile Application Development";
+
+// alert(`${studentName}`);
+// alert(`${age} years old`);
+// alert(`${course}`);
+
+//Question 5
+// let pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza);
+
+//Question 6
+// let email = "zohakhan@gmail.com";
+// alert(`My email address is ${email}`)
+
+//Question 7
+// let book = "A smarter way to learn Javascript";
+// alert(`I am trying to learn from the Book ${book}`);
+
+//Question 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+//Question 9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+//========================== Chapter 3 =================================
+
+//Question 1
+// let age = 20;
+// alert(`I am ${age} years old`);
+
+//Question 2
+//  let visitCount = localStorage.getItem('visitCount');
+
+// if (!visitCount) {
+//     visitCount = 0;
+// }
+// visitCount++;
+
+// localStorage.setItem('visitCount', visitCount);
+// document.write(`You have visited this site ${visitCount} times.`);
+// alert(`You have visited this site ${visitCount} times.`);
+
+//Question 3
+// let birthYear = 2004;
+// document.write(`My birth year is ${birthYear}`);
+
+//Question 4
+// let visitorName = "Jhon Doe";
+// let productTitle = "on XYZ Clothing store";
+// let quantity = "5 T-shirt(s)";
+
+// alert(`${visitorName} ordered ${quantity} ${productTitle}`);
+
+//=========================== Chapter 04 ==================================
+
+//Question 1
+// let name = "Zoha" , name2 = "Noor" , name3 = "Meerub" ;
+
+//Question 2
+//Legal variable names
+// let name;
+// let myName;
+// let $country;
+// let my_country;
+// let number123;
+
+//Illegal variable name
+// 2name
+// let
+// my-name
+// @username
+// null
+
+//Question 3
+// document.write("<h2>Rules for naming JS variable</h2>");
+
+// document.write("Variable names can only contain uppercase , lowercase , $ and _ <br>");
+// document.write("Variables must begin with a $name , _name or name <br>");
+// document.write("Variables name are case sensitive <br>");
+// document.write("Variables names should not be JS keywords <br>");
